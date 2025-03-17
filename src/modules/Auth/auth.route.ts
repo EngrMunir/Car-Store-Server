69f16978c5d46f478262f4594a370b6c9a53cb6b
munir <smsirajulmonir@gmail.com>
2025-03-17 14:49:51 +0600

    change password with refresh token addedd

--- src/modules/Auth/auth.route.ts
+++ src/modules/Auth/auth.route.ts
@@ -1,14 +1,52 @@
-import express from 'express';
+import { Router } from 'express';
 import { AuthControllers } from './auth.controller';
+import auth from '../../app/middleware/auth';
+import { USER_ROLE } from '../User/user.constant';
 import validateRequest from '../../app/middleware/validateRequest';
-import { AuthValidation } from './authValidation';
 
-const router = express.Router();
+const router = Router();
 
 router.post(
-    '/login',
-    validateRequest(AuthValidation.loginValidationSchema),
-    AuthControllers.loginUser,
-)
+  '/logout',
+  auth(USER_ROLE.admin, USER_ROLE.user),
+  AuthControllers.logoutUser,
+);
+
+router.post(
+  '/register',
+  validateRequest(AuthValidations.registerUserValidationSchema),
+  AuthControllers.registerUser,
+);
+
+router.post(
+  '/login',
+  validateRequest(AuthValidations.loginValidatioinSchema),
+  AuthControllers.loginUser,
+);
+
+router.post(
+  '/change-password',
+  auth(USER_ROLE.admin, USER_ROLE.user),
+  validateRequest(AuthValidations.changePasswordValidatioinSchema),
+  AuthControllers.changePassword,
+);
+
+router.post(
+  '/refresh-token',
+  validateRequest(AuthValidations.refreshTokenValidationSchema),
+  AuthControllers.refreshToken,
+);
+
+router.post(
+  '/forget-password',
+  validateRequest(AuthValidations.forgetPasswordValidationSchema),
+  AuthControllers.forgetPassword,
+);
+
+router.post(
+  '/reset-password',
+  validateRequest(AuthValidations.resetPasswordValidationSchem),
+  AuthControllers.resetPassword,
+);
 
 export const AuthRoutes = router;
